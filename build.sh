e35e11b801f2af944abc8cb8f8f5e2c739c85fae
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-04-07 17:58:03 +0300

    prepare for deploy

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python ./alfa_romeo_web/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python .alfa_romeo_web/manage.py migrate
