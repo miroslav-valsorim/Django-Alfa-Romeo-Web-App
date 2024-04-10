a16a0fa16f514ac2b3ddf59a8d9b7784c73dcfef
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-04-10 17:45:14 +0300

    password reset

--- alfa_romeo_web/alfa_romeo_web/accounts/urls.py
+++ alfa_romeo_web/alfa_romeo_web/accounts/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from alfa_romeo_web.accounts.views import (LoginUserView, RegisterUserView, logout_view, ProfileDetailsView,
                                            ProfileEditView, ProfileDeleteView, ProfileChangePasswordView,
                                            StaffPanelView)
 
 urlpatterns = (
-    path('staff_panel/', StaffPanelView.as_view(), name='staff_panel'),
+    # Password Reset from django
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
+    # Our Urls
+    path('staff_panel/', StaffPanelView.as_view(), name='staff_panel'),
     path("login/", LoginUserView.as_view(), name='login-user'),
     path('register/', RegisterUserView.as_view(), name='register-user'),
     path('logout/', logout_view, name='logout-user'),
