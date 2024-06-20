53293bd60d6e3a9e80d732b96faa3d3f29a84266
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-06-20 17:25:54 +0300

    add logs to the pipeline

--- alfa_romeo_web/alfa_romeo_web/settings.py
+++ alfa_romeo_web/alfa_romeo_web/settings.py
@@ -193,3 +193,17 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
