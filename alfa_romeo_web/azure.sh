db7e01e69eb7cace2442ca7a413012e5ec5e7b59
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-07-28 17:15:17 +0300

    another azure fix

--- alfa_romeo_web/azure.sh
+++ alfa_romeo_web/azure.sh
@@ -1,14 +1,4 @@
 
-python manage.py makemigrations
-
-sleep 20
-
-python manage.py migrate
-
-sleep 20
-
 python manage.py collectstatic --noinput
-
-sleep 20
 
 gunicorn --bind=0.0.0.0 alfa_romeo_web.wsgi
