2129a4854f7542ce50c542c4d32877fa8323e7c8
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-08-01 12:07:49 +0300

    another azure update

--- azure.sh
+++ azure.sh
@@ -3,6 +3,15 @@
 # Enter the alfa_romeo_web directory or exit if it fails
 cd alfa_romeo_web || { echo "Failed to enter alfa_romeo_web directory"; exit 1; }
 
+# Install requirements
+pip install -r requirements.txt
+
+# Makemigrations cos the PayPal ipn
+python manage.py makemigrations
+
+# Migrate all tables
+python manage.py migrate
+
 # Collect static files
 python manage.py collectstatic --noinput || { echo "Failed to collect static files"; exit 1; }
 
