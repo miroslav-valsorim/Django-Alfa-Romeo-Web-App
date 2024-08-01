260a14bd811f24c8fffa2dd4d1d7c8de15b6e1df
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-08-01 11:38:29 +0300

    addition to azure settings

--- azure.sh
+++ azure.sh
@@ -1,5 +1,10 @@
-cd alfa_romeo_web || exit
+#!/bin/sh
 
-python manage.py collectstatic --noinput
+# Enter the alfa_romeo_web directory or exit if it fails
+cd alfa_romeo_web || { echo "Failed to enter alfa_romeo_web directory"; exit 1; }
 
-gunicorn --bind=0.0.0.0 alfa_romeo_web.wsgi
+# Collect static files
+python manage.py collectstatic --noinput || { echo "Failed to collect static files"; exit 1; }
+
+# Start the gunicorn server
+gunicorn --bind=0.0.0.0 alfa_romeo_web.wsgi || { echo "Failed to start gunicorn"; exit 1; }
