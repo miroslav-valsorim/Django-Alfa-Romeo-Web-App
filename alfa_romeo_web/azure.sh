fb62dd636398ca58a366a82116cd6c33ac71dc6c
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-08-07 10:37:30 +0300

    update inner azure.sh

--- alfa_romeo_web/azure.sh
+++ alfa_romeo_web/azure.sh
@@ -1,4 +1,14 @@
+# Install requirements
+pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+# Makemigrations cos the PayPal ipn
+python manage.py makemigrations
 
-gunicorn --bind=0.0.0.0 alfa_romeo_web.wsgi
+# Migrate all tables
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput || { echo "Failed to collect static files"; exit 1; }
+
+# Start the gunicorn server
+gunicorn --bind=0.0.0.0 alfa_romeo_web.wsgi || { echo "Failed to start gunicorn"; exit 1; }
